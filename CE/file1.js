12c306ce53c29ec0a0bf4925ef6d7d93a39e4c85
Dr. AJ <09.aatif@gmail.com>
2025-02-05 11:20:06 +0530

    React Files

--- CE/file1.js
+++ CE/file1.js
@@ -1,3 +1,14 @@
 const fs = require('fs');
-const data = fs.readFileSync("./data.txt","utf-8");
+const data = fs.readFileSync("./data.txt", "utf-8");
 console.log(data);
+if (data.match("H")) {
+  console.log("H is present");
+  const changedata = data.replace("H", "ABES");
+  fs.writeFileSync("./data.txt", changedata);
+}
+
+// fs.unlinkSync("./data2.txt");
+
+// fs.writeFileSync("./data1.txt", "ABSE promise");
+// fs.appendFileSync("./data1.txt", "AKTU");
+// fs.renameSync("./data1.txt", "./data2.txt")
