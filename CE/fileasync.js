e2d1ecf5277707278ef04b469dadfa7afaeb3f52
Dr. AJ <09.aatif@gmail.com>
2025-02-07 09:23:24 +0530

    updated work 7/2/25

---
+++ CE/fileasync.js
@@ -0,0 +1,20 @@
+const fs = require('fs').promises;
+
+const write = async () => {
+  const newData = "This is new data being written!";
+  await fs.writeFile("./data.txt", newData, { encoding: "utf8" });
+  console.log("Data written successfully!");
+};
+
+const read = async () => {
+  const data = await fs.readFile("./data.txt", { encoding: "utf8" });
+  console.log(data);
+};
+
+const main = async () => {
+  await write(); // Write to the file first
+  await read();  // Read from the file after writing
+};
+
+main();
+console.log("Code Ended");
