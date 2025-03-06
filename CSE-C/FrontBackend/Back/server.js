085190ad786bc4e16dc985fe04ebb2c0bbc06bf2
Dr. AJ <09.aatif@gmail.com>
2025-03-06 15:51:13 +0530

    6/3

---
+++ CSE-C/FrontBackend/Back/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors'; // npm i cors
+const app = express();
+const port = 3000;
+app.use(cors()); //Built in middlware
+app.get("/api", async (req, res) => {
+  const apidata = await axios.get("https://dummyjson.com/products");
+  res.send(apidata.data.products);
+});
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ CSE-C/FrontBackend/Front/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'; //npm i axios
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const apidata = await axios.get("http://localhost:3000/api");
+      setProducts(apidata.data);
+    }
+    fetchdata();
+  }, [])
+
+  return (
+    <div>
+      <h1>All the product details are given below:</h1>
+      {/* {JSON.stringify(products)} */}
+      {products.map((product) => {
+        return (
+          <div>
+            <h3>{product.title}</h3>
+            <img src={product.thumbnail}></img>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+export default App
